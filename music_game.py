d40f628b78dff07721e3f93857ea03ac2216f55b
Harrison-Finkbeiner <harrison.finkbeiner@gmail.com>
2025-08-06 18:59:25 -0700

    Continued development

--- music_game.py
+++ music_game.py
@@ -18,4 +18,46 @@ class MusicGame():
         self.con = imaplib.IMAP4_SSL(imap_url)
 
         self.con.login(user_email, password)
-        
+
+        try:
+            with open('players.json', 'r') as file:
+                data = json.load(file)
+            
+
+        except FileNotFoundError:
+            print("Error: 'players.json' not found.")
+        except json.JSONDecodeError:
+            print("Error: Invalid JSON format in 'players.json'.")
+
+        self.game_code = data.get("game_code", None)
+
+
+
+    def getMessages(self):
+        self.con.select('INBOX')
+        status, data = self.con.search(None, "SUBJECT", str(self.game_code))
+
+        if status == 'OK':
+            message_ids = data[0].split()
+
+            if message_ids:
+                for msg_id in message_ids:
+                    status, msg_data = self.con.fetch(msg_id, '(RFC822)')
+                    if (status == 'OK'):
+                        raw_email = msg_data[0][1]
+                        msg = email.message_from_bytes(raw_email)
+
+                        print(msg)
+            else:
+                print(f"No messages found with subject {self.game_code}")
+        else:
+            print(f"Failed to search with status {status}"
+
+
+def main():
+
+    mg = MusicGame()
+    mg.getMessages()
+
+if __name__ == "__main__":
+    main()
